10e6652fb8ef9c7d4733a41e3a0571aa44451d78
JJOB73 <a20212529@pucp.edu.pe>
2025-04-09 19:34:57 -0500

    primer commit

---
+++ src/main/java/com/example/tareaclase3/controller/Controller.java
@@ -0,0 +1,21 @@
+package com.example.tareaclase3.controller;
+
+import com.example.tareaclase3.entity.Employee;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@org.springframework.stereotype.Controller
+@RequestMapping("")
+public class Controller {
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/index}";
+    }
+
+    @GetMapping("/index")
+    public String showConfigPage() {
+        return "index";
+    }
+
+
+}
